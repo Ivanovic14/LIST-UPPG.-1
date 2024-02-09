ea4ce4a9ef714b1836ecf5a035b415a18d5013d1
Ivanovic14 <ival06001@utb.vaxjo.se>
2024-02-09 10:42:37 +0100

    Add project files.

---
+++ LIST UPPG. 1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace LIST_inl.Uppg_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> namnen = new List<string>();
+
+            Console.WriteLine("Skriv namnen på människorna som du älskar.");
+            Console.WriteLine("AVLUSTA MED ETT TOMT RAD.");
+            string svarnamn = " ";
+
+            while (svarnamn != "")
+            {
+                Console.WriteLine("Skriv in ett nytt namn.");
+                svarnamn = Console.ReadLine();
+                if (svarnamn != "")
+                {
+                    namnen.Add(svarnamn);
+                }
+            }
+            Console.WriteLine("Här är namnen du angav:");
+
+            namnen.Sort();
+            foreach (string resultat in namnen)
+            {
+                Console.WriteLine(resultat);
+            }
+
+        }
+
+    }
+}
+
